646cc83f680c4eca652f6cf02c9ba55be33b2a14
meade-rice <44656018+meade-rice@users.noreply.github.com>
2021-02-09 14:29:58 -0800

    Update plotResPages.m

--- AOX_RequiredSupportFiles/plotResPages.m
+++ AOX_RequiredSupportFiles/plotResPages.m
@@ -35,31 +35,30 @@ end
 
 %Generate subplot for each series for residuals: Max of 6 subplots per
 %Figure window
-sub = 0;
-r = min(n_dim,6);
+%sub = 0;
+%r = min(n_dim,6);
+t = tiledlayout(n_dim, 1);
 for i = 1:n_dim %subplot for each series
-    if i>1 && rem(i,6) == 1 %If first subplot for new window
-        h1=gcf;
-        figure('Name',h1.Name,'NumberTitle','off','WindowState','maximized');
-        sub = 6;
+%    if i>1 && rem(i,6) == 1 %If first subplot for new window
+%        h1=gcf;
+%        figure('Name',h1.Name,'NumberTitle','off','WindowState','maximized');
+%        sub = 6;
         %         r = n_dim - 6;
-        r = 6;
-    end
-    subplot(r, 1, i-sub); hold on
-    if exist('loadCapacities','var')
-        axis([1 n_data -1 1]);
-    else
-        xlim([1 n_data]);
-    end
+%        r = 6;
+%    end
+%    subplot(r, 1, i-sub); hold on
     
+    
+    nexttile(t);
     %Shades blocks to distinguish series
     for j = 1:n_series
         if mod(j,2) == 0
             patch([ind_s(j,1)-0.5 ind_s(j,2)+0.5 ind_s(j,2)+0.5 ind_s(j,1)-0.5], [-1 -1 1 1], [0.8 0.8 0.8])
         end
     end
-    
+    hold on
     plot([1:n_data],targetResPct(:,i)) %Plot residuals
+    hold on
     if exist('loadCapacities','var')
         plot([1:n_data],thr*ones(1,n_data),'k--',[1:n_data],-thr*ones(1,n_data),'k--') %Plot desired threshold cutoff
     end
@@ -71,6 +70,12 @@ for i = 1:n_dim %subplot for each series
         title(sprintf('Residual; Standard Deviation = %0.4f',stdDevPercentCapacity(i)));
     end
     %     set(gcf,'Position',[100 50 825 min(r*175.5,900)]);
+    
+    if exist('loadCapacities','var')
+        axis([1 n_data -1 1]);
+    else
+        xlim([1 n_data]);
+    end
     hold off
 end
 
